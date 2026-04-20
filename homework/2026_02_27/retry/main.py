07545f2d3d797659b7f2d535cbe11a384c091a37
Volodia5 <80164552+Volodia5@users.noreply.github.com>
2026-04-20 23:38:06 +0200

    Update main.py

--- homework/2026_02_27/retry/main.py
+++ homework/2026_02_27/retry/main.py
@@ -1,5 +1,4 @@
 from random import random, choice
-
 from my_decorator import retry
 
 exceptions = (ValueError, OSError)
@@ -10,7 +9,6 @@ def random_exception() -> float:
         raise choice(exceptions)
     return d
 
-
 if __name__ == "__main__":
     for _ in range(100):
-        print("result:", random_exception())
+        print("итого:", random_exception())
