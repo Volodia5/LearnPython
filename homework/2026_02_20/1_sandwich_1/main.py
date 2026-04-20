33d7a406548d3f17c5dd909bb1990138df63cc31
Volodia5 <80164552+Volodia5@users.noreply.github.com>
2026-04-20 23:35:12 +0200

    Update main.py

--- homework/2026_02_20/1_sandwich_1/main.py
+++ homework/2026_02_20/1_sandwich_1/main.py
@@ -1,20 +1,33 @@
 def bread(func):
-    pass
-
-def salat(func):
-    pass
-
-def tomato(func):
-    pass
+    def wrapper():
+        print("Bread")
+        func()
+        print("Bread")
+    return wrapper
 
 def meat(func):
-    pass
+    def wrapper():
+        func()
+        print("Meat")
+    return wrapper
 
+def tomato(func):
+    def wrapper():
+        func()
+        print("Tomato")
+    return wrapper
+
+def salat(func):
+    def wrapper():
+        func()
+        print("Salat")
+    return wrapper
+
+@bread
+@salat
+@tomato
+@meat
 def make_sandwich():
     pass
 
-def main():
-    make_sandwich()
-
-if __name__ == '__main__':
-    main()
+make_sandwich()
