05b3bbc7fee206861fa0e7adfbd6b4fb940f0f84
Volodia5 <80164552+Volodia5@users.noreply.github.com>
2026-04-20 23:36:18 +0200

    Update main.py

--- homework/2026_02_20/3_our_cache/main.py
+++ homework/2026_02_20/3_our_cache/main.py
@@ -1,5 +1,18 @@
 def cache(func):
-    pass
+    storage = {}
 
-def my_sum(a, b):
+    def wrapper(*args):
+        if args not in storage:
+            storage[args] = func(*args)
+        return storage[args]
+
+    return wrapper
+
+@cache
+def slow_add(a, b):
+    print(f"считаем {a}+{b}")
     return a + b
+
+print(slow_add(1, 2))
+print(slow_add(1, 2))
+print(slow_add(3, 4))
